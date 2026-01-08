fac2b90a3a925c11304b82f9e99a5ff0fed2b9c3
ElektrifiedDev <160866233+ElektrifiedDev@users.noreply.github.com>
2026-01-08 01:05:10 +0000

    Update main.py
    Added standard library and colorama imports, including initialization with autoreset enabled. This sets up the environment for further development.

---
+++ main.py
@@ -0,0 +1,13 @@
+import random as rand
+import collections
+import os
+import json
+import sys
+import time
+import datetime
+import colorama
+import hashlib
+
+from colorama import Fore, Style, init
+
+init(autoreset=True)
